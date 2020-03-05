99e568c3aa2b92d3dbc7b38ae37008afded2c959
Jake Afuvai <jtafuvai@gmail.com>
2020-03-04 18:02:09 -0700

    first commit to first electron desktop app a la reddit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = props => {
+    const [ posts, setPosts ] = useState([])
+
+    useEffect(() => {
+        axios.get("https://reddit.com/r/funny.json")
+            .then(response => {
+                setPosts(response.data.data.children)
+            })
+            .catch(error => {
+                console.log(error)
+            })         
+    }, [])
+
+    console.log(posts)
+
+    const postsDisplay = posts.map(post => 
+            <li 
+                key={post.data.id}
+                className="post-list-item"
+            >
+                {`${post.data.author} `}
+                {post.data.title}
+                {/* <img src={post.data.preview.images[0].source.url} alt="no image available" /> */}
+            </li>
+        )
+    
+    return (
+        <div className="app-container">
+            {postsDisplay} 
+        </div>
+    )
 }
 
-export default App;
+export default App
